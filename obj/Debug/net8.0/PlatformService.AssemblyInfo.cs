372d6b3f3702fd6b420b75aec7e3a79101287c75
sulakshigunarathne <gunarathneawsm.20@uom.lk>
2024-02-17 22:21:10 +0530

    Dockerfile created

--- obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+533e4747df2970ac8698dc7989bba326e7110257")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11cee6b23a16761c9b8e5c634a04442bc57d92e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
