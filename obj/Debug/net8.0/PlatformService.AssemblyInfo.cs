13b1f10f51c5aeb3a02567a01a0deb0133164f87
sulakshigunarathne <gunarathneawsm.20@uom.lk>
2024-03-05 15:38:28 +0530

    PlatformService

--- obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84566284cdba6abffa3cf8ad4bd5f0bfcd5edb11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fa099f380b31710f19e9e46500cc9b1f832b63b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
